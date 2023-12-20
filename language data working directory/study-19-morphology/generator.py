226265f5a792fd0b1280685e6c2fa010f4a54ad5
pajubrat <pauli.j.brattico@gmail.com>
2023-12-20 12:03:07 +0100

    Study 19

--- language data working directory/study-19-morphology/generator.py
+++ language data working directory/study-19-morphology/generator.py
@@ -1,14 +1,14 @@
 # This script generates the dataset for Study 19
 
-roots = ['pino-', 'laula-', 'luke-', 'puna-']
+roots = ['pino-', 'istu-', 'luKe-', 'iso-']
 
-inner_V_morphemes = ['TA#v', 'ile', 'UTU', 'AhtA', 'a#v']
-inner_N_morphemes = ['O', 'Us', 'ntO', 'stO', 'e']
+inner_V_morphemes = ['TA', 'ile', 'UTU', 'AhtA', 'Aise', 'a']
+inner_N_morphemes = ['O', 'Us', 'stO', '(t)e']
 inner_A_morphemes = ['ise', 'mAise']
 
 outer_V_morphemes = ['(t)A', 'impss', 'T/fin#prs#n', 'v']
-outer_N_morphemes = ['mise', 'tAr', 'jA', 'ri', '[NOM´]']
-outer_A_morphemes = ['inen', 'mAinen', 'llinen', 'mAtOn', 'mA´', 'vA', 'hkO']
+outer_N_morphemes = ['mise', 'tAr', 'jA', 'ri', '[NOM]']
+outer_A_morphemes = ['inen', 'mAinen', 'mAtOn', 'tOn', 'mA', 'vA', 'hkO']
 
 inner_morphemes = inner_V_morphemes + inner_N_morphemes + inner_A_morphemes
 outer_morphemes = outer_V_morphemes + outer_N_morphemes + outer_A_morphemes
@@ -19,6 +19,7 @@ MAX_SUFFIXES = 3
 
 # This will store the  generated words
 items = []
+
 
 def add_suffixes(length, w, count):
 
@@ -47,8 +48,9 @@ for total_suffixes in range(0, MAX_SUFFIXES):
 
 # Store the results into file
 print(f'\nGenerated {len(items)} items, storing them to file...')
-output_file = open('GENERATED_CORPUS.txt', 'w', -1, 'utf-8')
+output_file = open('Generated_corpus.txt', 'w', -1, 'utf-8')
 output_file.write('# Generated corpus\n')
-for item in items:
-    output_file.write(f'\n\n{item}')
+for i, item in enumerate(items):
+    output_file.write(f'# {i+1}')
+    output_file.write(f'\n{item}\n\n')
 output_file.close()
